3c6d45601ebfbfe1324d02fd115dceae5a97e647
Nayeem Basha <mnayeembasha@gmail.com>
2025-03-17 22:01:11 +0530

    fixed cors issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,22 +9,22 @@ import { evaluateScore } from "./controllers/evaluateScore";
 
 const app: express.Application = express();
 
-// const allowedOrigins = ["http://localhost:5173", "https://bingo-blitz.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "https://bingo-blitz.vercel.app"];
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 
 app.use(express.json());
